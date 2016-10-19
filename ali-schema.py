13fb86614636a1926d450e6a8ac83a1baf89acca
Luka Jankovic <lukjan1999@gmail.com>
2016-10-20 00:26:22 +0200

    fixed menu buttons

--- ali-schema.py
+++ ali-schema.py
@@ -104,11 +104,25 @@ class ALISchemaApplication(Gtk.Application):
 
         win.reload()
 
+    def on_quit(self, action, param):
+        self.quit()
+
+    def show_settings(self, action, param):
+        print("test")
+
     def do_startup(self):
         Gtk.Application.do_startup(self)
 
         builder = Gtk.Builder()
         builder.add_from_file("menus.ui")
+
+        settings_action = Gio.SimpleAction.new("settings", None)
+        settings_action.connect("activate", self.show_settings)
+        self.add_action(settings_action)
+
+        exit_action = Gio.SimpleAction.new("quit", None)
+        exit_action.connect("activate", self.on_quit)
+        self.add_action(exit_action)
 
         self.set_app_menu(builder.get_object("app-menu"))
 
